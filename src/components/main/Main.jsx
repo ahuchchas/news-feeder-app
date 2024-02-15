372c7000afcd285bcf251ca3019c922c36cb5a6e
Abu Hurayra Uchchas <ahuchchas@outlook.com>
2024-02-15 23:28:29 +0600

    error msg style added

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -14,7 +14,7 @@ export default function Main() {
   return (
     <main className="my-10 lg:my-14">
       <div className="container mx-auto grid grid-cols-12 gap-8">
-        {error && <p>{error.message}</p>}
+        {error && <p className="text-xl text-center mt-12">{error.message}</p>}
 
         {loading.state && (
           <p className="text-xl text-center mt-12">{loading.message}</p>
